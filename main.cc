933660345705bd6a473f05c5fee7cf238bbf494f
zingaburga <zingaburga@hotmail.com>
2022-10-25 14:47:58 +1000

    CLI improvements

--- main.cc
+++ main.cc
@@ -1,6 +1,4 @@
-#ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS 1
-#endif
 
 #include "include/simdflate.h"
 #include "include/simdflate_cpucheck.h"
@@ -10,16 +8,42 @@
 #include <string.h>
 #include <chrono>
 
+void print_usage(const char* bin) {
+	fprintf(stderr, "Usage: %s [options] input output\n", bin);
+	fprintf(stderr, "  -r<n>    Run <n> repetitions\n");
+	fprintf(stderr, "  -z       Zlib format instead of gzip\n");
+	fprintf(stderr, "  -d       Raw Deflate instead of gzip\n");
+}
 
 int main(int argc, char** argv) {
 	fprintf(stderr, "SIMDflate support check: compiler=%s, CPU=%s\n", SIMDFLATE_COMPILER_SUPPORTED ?"yes":"no", simdflate_cpu_support() ?"yes":"no");
 	if(argc < 3) {
-		fprintf(stderr, "Usage: %s input output\n", argv[0]);
+		print_usage(argv[0]);
 		return 1;
 	}
 	
 	int reps = 1;
-	auto f = fopen(argv[1], "rb");
+	int format = 0; // gzip
+	char** argp = argv + 1;
+	while(argc-- > 3) {
+		if((*argp)[0] != '-') {
+			print_usage(argv[0]);
+			return 1;
+		}
+		switch((*argp)[1]) {
+			case 'r':
+				reps = atoi((*argp) + 2);
+				break;
+			case 'z': format = 1; break;
+			case 'd': format = 2; break;
+			default:
+				print_usage(argv[0]);
+				return 1;
+		}
+		argp++;
+	}
+	
+	auto f = fopen(argp[0], "rb");
 	if(!f) {
 		fprintf(stderr, "Failed to open input file: %s\n", strerror(errno));
 		return 1;
@@ -34,20 +58,43 @@ int main(int argc, char** argv) {
 	}
 	fclose(f);
 	
+	float us;
+	size_t output_len;
 	std::vector<unsigned char> output(simdflate_max_gzip_len(data.size()));
 	
-	auto start = std::chrono::high_resolution_clock::now();
-	auto output_len = simdflate_gzip(output.data(), data.data(), data.size());
-	for(int i=1; i<reps; i++)
-		simdflate_gzip(output.data(), data.data(), data.size());
-	auto end = std::chrono::high_resolution_clock::now();
+	if(format == 0) {
+		auto start = std::chrono::high_resolution_clock::now();
+		output_len = simdflate_gzip(output.data(), data.data(), data.size());
+		for(int i=1; i<reps; i++)
+			simdflate_gzip(output.data(), data.data(), data.size());
+		auto end = std::chrono::high_resolution_clock::now();
+		
+		us = float(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
+	}
+	else if(format == 1) {
+		auto start = std::chrono::high_resolution_clock::now();
+		output_len = simdflate_zlib(output.data(), data.data(), data.size());
+		for(int i=1; i<reps; i++)
+			simdflate_zlib(output.data(), data.data(), data.size());
+		auto end = std::chrono::high_resolution_clock::now();
+		
+		us = float(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
+	}
+	else if(format == 2) {
+		auto start = std::chrono::high_resolution_clock::now();
+		output_len = simdflate_deflate(output.data(), data.data(), data.size());
+		for(int i=1; i<reps; i++)
+			simdflate_deflate(output.data(), data.data(), data.size());
+		auto end = std::chrono::high_resolution_clock::now();
+		
+		us = float(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
+	}
 	
 	auto input_len = double(data.size());
-	auto us = float(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
-	printf("Ratio: %.3f%%\n", (double(output_len) / input_len) * 100);
-	printf("Speed: %.3f MiB/s\n", input_len*reps/1.048576 / us);
+	printf("Size: %zu bytes (%.3f%%)\n", output_len, (double(output_len) / input_len) * 100);
+	printf("Time: %.3f s (%.3f MiB/s)\n", us/1000000, input_len*reps/1.048576 / us);
 	
-	f = fopen(argv[2], "wb");
+	f = fopen(argp[1], "wb");
 	if(!f) {
 		fprintf(stderr, "Failed to open output file: %s\n", strerror(errno));
 		return 1;
